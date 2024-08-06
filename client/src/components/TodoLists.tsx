7acc803ae6eaf9febdad74ac10047aee1e1d94be
Eljo George <eljog@github.com>
2024-08-06 08:02:42 +0000

    delete icon

--- client/src/components/TodoLists.tsx
+++ client/src/components/TodoLists.tsx
@@ -11,35 +11,40 @@ import { TodoItem } from '@todo-app/common'
 import { getApiClient } from '../api';
 import Alert from '@mui/material/Alert';
 import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const label = { inputProps: { 'aria-label': 'Todo completed or not' } };
 
 
-export default function TodoLists(data: {todos: TodoItem[]}) {
-  const {todos} = data
-  return (     
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell align="right">Completed</TableCell>
+export default function TodoLists(data: { todos: TodoItem[] }) {
+  const { todos } = data
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Completed</TableCell>
+            <TableCell align="right">Delete?</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {todos.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.title}
+              </TableCell>
+              <TableCell align="right"><Checkbox {...label} checked={row.completed} /></TableCell>
+              <TableCell align="right"><Button color='inherit' startIcon={<DeleteIcon />}>
+              </Button></TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {todos.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.title}
-                </TableCell>
-                <TableCell align="right"><Checkbox {...label} checked={row.completed} /></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
