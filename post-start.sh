081353de6d670e3ae8c778b8c02723bcebc3674c
Eljo George <eljog@github.com>
2024-08-06 07:43:22 +0000

    devcontainer

---
+++ post-start.sh
@@ -0,0 +1,4 @@
+gh codespace ports -c $CODESPACE_NAME visibility 3001:public && true
+npm i
+npm run build
+npm run start
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import cors from 'cors'
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const server = getServer()
 
